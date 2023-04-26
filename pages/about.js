cb2942c4a26757c56358409fc3e2d5e09239bd3a
higorseixas <higor.c.seixas@gmail.com>
2023-04-25 23:15:24 -0300

    Adicionando texto

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className={styles.about}>
       <h1>Sobre o projeto</h1>
-      <p>Lorem, </p>
+      <p>PokéNet é uma enciclopédia virtual que contém informações detalhadas sobre cada Pokémon existente. É um recurso valioso para treinadores de Pokémon e fãs da franquia, pois permite que eles conheçam cada criatura, suas características e habilidades.</p>
       <Image 
       src="/images/charizard.png" 
       width="300" 
